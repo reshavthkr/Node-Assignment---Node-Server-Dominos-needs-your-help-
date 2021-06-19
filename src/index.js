810e42abf1251cec0dc390a101c73a3f067cac74
Reshav Thakur <Reshavthkr@gmail.com>
2021-06-19 16:06:18 +0530

    New file is created

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if (req.url === "/welcome"){
+        res.setHeader("Content-Type", "text/plain");
+        res.writeHead(200);
+        res.end("Welcome to Dominos!");
+    }
+    if (req.url === "/contact"){
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(`{
+        "phone":"18602100000",
+        "email": "guestcaredominos@jublfood.com"}`);
+    }
+    else{
+        res.statusCode = 404;
+        res.end();
+    }
 }
-
+httpServer.listen(8081);
+console.log("Listening to port 8081.....");
 module.exports = httpServer;
